88f70b3f675f3a2edf24404fa2d710aca4dee0df
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-08 18:42:13 -0400

    common header for asistente pages

---
+++ asistente/header-common.php
@@ -0,0 +1,13 @@
+<?php
+    
+require_once(dirname(__FILE__).'/../includes/lib.php');
+
+beginSession('A');
+do_header();
+?>
+<div id="login-info">
+    <p class="yacomas_login">
+    Login: <?=$_SESSION['YACOMASVARS']['asilogin'] ?> | 
+    <a class="precaucion" href="signout.php">Desconectarme</a>
+    </p>
+</div>
