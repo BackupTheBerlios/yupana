8c8735c5a8545a30cb0b4aff41bd08728dbeebeb
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-09 07:03:14 -0400

    added month2name function

--- includes/lib.php
+++ includes/lib.php
@@ -328,6 +328,28 @@ function err ($errmsg) {
   exit;
 }
 
+function month2name ($id) {
+    $result = $id;
+    $id = (int) $id;
+
+    switch ($id) {
+        case  1: $result = 'Enero'; break;
+        case  2: $result = 'Febrero'; break;
+        case  3: $result = 'Marzo'; break;
+        case  4: $result = 'Abril'; break;
+        case  5: $result = 'Mayo'; break;
+        case  6: $result = 'Junio'; break;
+        case  7: $result = 'Julio'; break;
+        case  8: $result = 'Agosto'; break;
+        case  9: $result = 'Septiembre'; break;
+        case 10: $result = 'Octubre'; break;
+        case 11: $result = 'Noviembre'; break;
+        case 12: $result = 'Diciembre'; break;
+    }
+
+    return $result;
+}
+
 // DELETEME: BD conection moved to adodb
 function conectaBD()
 {
