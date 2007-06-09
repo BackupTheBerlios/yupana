b726e4044d785fce70c68e10daf0b0fb46ff41d9
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-09 09:06:12 -0400

    cleanup & update

--- admin/Lponentes.php
+++ admin/Lponentes.php
@@ -1,63 +1,57 @@
 <?php
-require_once('header-common.php');
+    require_once('header-common.php');
 
-$query = 'SELECT P.id, P.login, P.nombrep, P.apellidos,
-        P.reg_time,  E.descr AS estado, ES.descr AS estudios 
-		FROM ponente AS P, estado AS E, estudios AS ES 
-		WHERE P.id_estado=E.id AND P.id_estudios=ES.id 
-		ORDER BY P.id,P.reg_time';
-
-$users = get_records_sql($query);
-$total_ponentes = count_records('ponente');
-
+    $request_uri = $_SERVER['REQUEST_URI'];
+    $rootlevel = $_SESSION['YACOMASVARS']['rootlevel'];
+    $total_ponentes = count_records('ponente');
 ?>
 
 <h1>Listado de ponentes registrados</h1>
-<h5><?=$total_ponentes ?> Ponentes Registrados</h5>
-
-<table class="wide">
-    <tr class="table-headers">
-        <td>Nombre</td>
-        <td>Login</td>
-        <td>Estado</td>
-        <td>Estudios</td>
-        <td>Registro</td>
+<h3><?=$total_ponentes ?> Ponentes Registrados</h3>
 
 <?php
-	if ($_SESSION['YACOMASVARS']['rootlevel']==1) {
-?>
-        <td></td>
-<?php
+$query = 'SELECT P.id, P.login, P.nombrep, P.apellidos,
+    P.reg_time,  E.descr AS estado, ES.descr AS estudios
+    FROM ponente AS P, estado AS E, estudios AS ES
+    WHERE P.id_estado=E.id AND P.id_estudios=ES.id
+    ORDER BY P.id,P.reg_time';
+
+$users = get_records_sql($query);
+
+if (!empty($users)) {
+    $table_data = array();
+
+    $table_headers = array('Nombre', 'Login', 'Departamento', 'Estudios', 'Registro');
+    if ($rootlevel == 1) {
+        $table_headers = array_merge($table_headers, array('Acción'));
     }
-?>
-    </tr>
-<?php
-    if (!empty($users)) {
-        $trclass = 'even';
 
-        foreach ($users as $user) {
-?>
-    <tr class="<?=($trclass == 'even') ? 'even' : 'odd' ?>">
-        <td><a class="azul" href="Vponente.php?vopc=<?=$user->id ?> <?=$_SERVER['REQUEST_URI'] ?>"><?=$user->nombrep ?> <?=$user->apellidos ?></a></td>
-        <td><?=$user->login ?></td>
-        <td><?=$user->estado ?></td>
-        <td><?=$user->estudios ?></td>
-        <td><?=$user->reg_time ?></td>
-<?php
-        if ($_SESSION['YACOMASVARS']['rootlevel'] == 1) {
-?>
-        <td><a class="precaucion" href="Bponente.php?idponente=<?=$user->id ?>">Eliminar</a></td>
-<?php
+    $table_data[] = $table_headers;
+
+    foreach ($users as $user) {
+        $l_nombre = <<< END
+<a class="azul" href="Vponente.php?vopc={$user->id} {$request_uri}">{$user->nombrep} {$user->apellidos}</a>
+END;
+        $row_data = array($l_nombre, $user->login, $user->estado, $user->estudios, $user->reg_time);
+
+        if ($rootlevel == 1) {
+            $l_action = <<< END
+<a class="precaucion" href="Bponente.php?idponente={$user->id}">Eliminar</a>
+END;
+            $row_data = array_merge($row_data, array($l_action));
         }
-?>
-    </tr>
-<?php
-            // toggle trclass
-            $trclass = ($trclass == 'even') ? 'odd' : 'even';
-        }
+
+        $table_data[] = $row_data;
     }
+
+    do_table($table_data, 'wide');
+
+} else {
 ?>
-</table>
+
+<p class="error center">No se tiene ningún ponente registrado.</p>
+
+<?php } ?>
 
 <p id="buttons">
     <input type="button" value="Volver al menu" onClick="location.href='<?=$CFG->wwwroot ?>/admin/menuadmin.php#ponencias'" />
