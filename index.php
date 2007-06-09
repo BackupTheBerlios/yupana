736e24df87ca97714efa85876af3ab1d64f3607b
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-08 20:42:38 -0400

    new index

--- index.php
+++ index.php
@@ -1,49 +1,42 @@
 <?
-	include_once "includes/conf.inc.php";
-	include_once "includes/lib.php";
-	imprimeEncabezado();
-    #imprimeCajaTop("100","Registro");
+	require_once('includes/lib.php');
+
+    global $CFG;
+
+	do_header();
+?>
+<div id="frontpage">
+
+    <h1>Registro</h1>
+
+    <p>Gracias por tu inter&eacute;s en <?=$CFG->conference_name ?></p>
 
 
-print " 
-<p>Gracias por tu inter&eacute;s en $conference_name</p>
-";
-print '
- 
+ <h3><a href="<?=$CFG->wwwroot ?>/ponente/index.php?opc=<?=NPONENTE ?>">Registro de ponentes</a> - 
+    <a href="<?=$CFG->wwwroot ?>/ponente/">Accede a tu cuenta </a></h3>
+
  <p>
- <a href="'.$fslpath.$rootpath.'/ponente/index.php?opc=';
- print NPONENTE;
- print '">Registro de ponentes 
- </a>
- &nbsp;
- <a href="'.$fslpath.$rootpath.'/ponente/">Accede a tu cuenta
- </a>
-
- <br/>
  Es necesario tu registro, mediante el cual podr&aacute;s enviar
- ponencias y estar informado del evento.</p>
- <p>
- <a href="'.$fslpath.$rootpath.'/asistente/index.php?opc=';
- print NASISTENTE;
- print '">Registro de asistentes
- </a>
- &nbsp;
- <a href="'.$fslpath.$rootpath.'/asistente/">Accede a tu cuenta
- </a>
- <br/>
- Es necesario tu registro, mediante el cual podr&aacute;s realizar preinscripci&oacute;n al al congreso y  talleres
- ademas de mantenerte informado del evento.</p>
- ';
-// <p><a href="'.$fslpath.$rootpath.'/lista/">Lista preliminar de ponencias</a>
-// <br/>
-// Aqu&iacute; ver&aacute;s las propuestas ponencias que han sido enviadas, y el status en el que se encuentran dichas ponencias.</p>
- print '
-<p><a href="'.$fslpath.$rootpath.'/modalidades/">Modalidades de participacion en la peticion de ponencias</a>
- Modalidades de las ponencias que encontraras en el evento!
- <br />
- ';
+ ponencias y estar informado del evento.
+</p>
 
- #imprimeCajaBottom(); 
- ?>
+<h3>
+ <a href="<?=$CFG->wwwroot ?>/asistente/index.php?opc=<?=NASISTENTE ?>">Registro de asistentes </a> - <a href="<?=$CFG->wwwroot ?>/asistente/">Accede a tu cuenta </a></h3>
+
+ <p>
+ Es necesario tu registro, mediante el cual podr&aacute;s realizar preinscripci&oacute;n al al congreso y  talleres
+ ademas de mantenerte informado del evento.
+</p>
+
+<h3><a href="<?=$CFG->wwwroot ?>/lista/">Lista preliminar de ponencias</a></h3>
+
+<p>Aqu&iacute; ver&aacute;s las propuestas ponencias que han sido enviadas, y el status en el que se encuentran dichas ponencias.</p>
+
+<h3><a href="<?=$CFG->wwwroot ?>/modalidades/">Modalidades de participacion</a></h3>
+<p>
+Modalidades de las ponencias que encontraras en el evento!
+</p>
  
-<?imprimePie();?> 
+</div>
+
+<?=do_footer();?> 
