cf421f0887f45fbc735d0395cbb0c8e46affa00d
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-09 04:08:28 -0400

    cleanup & updates on listings rooms

--- admin/Llugar.php
+++ admin/Llugar.php
@@ -1,60 +1,57 @@
 <?php
-require_once('header-common.php');
-$rooms = get_records('lugar');
+    require_once('header-common.php');
+
+    $rootlevel = $_SESSION['YACOMASVARS']['rootlevel'];
 ?>
 
 <h1>Listado de lugares para eventos</h1>
 
-<table class="wide">
-    <tr class="table-headers">
-
-    <td>Nombre</td>
-    <td>Ubicación</td>
-    <td>Cupo</td>
-    <td>Eventos</td>
-
 <?php
-	if ($_SESSION['YACOMASVARS']['rootlevel']==1 ) {
-?>
-	<td></td>
-<?php
+$rooms = get_records('lugar');
+
+if (!empty($rooms)) {
+    $table_data = array();
+
+    $table_header = array('Nombre', 'Ubicación', 'Cupo', 'Eventos');
+    if ($rootlevel == 1) {
+        $table_header = array_merge($table_header, array('Acción'));
     }
-?>
-    </tr>
 
-<?php
-    if (!empty($rooms)) {
-        $trclass = 'even';
-        foreach ($rooms as $room) {
-?>
-    <tr class=<?=($trclass == 'even') ? 'even' : 'odd' ?>>
-    <td><a class="verde" href="Mlugar.php?idlugar=<?=$room->id ?>"><?=$room->nombre_lug ?></a></td>
-    <td><?=$room->ubicacion ?></td>
-    <td><?=($room->cupo != 99999) ? $room->cupo : '' ?></td>
-    <td><a class="azul" href="Leventos-lugar.php?idlugar=<?=$room->id ?>">Eventos Registrados</a></td>
+    $table_data[] = $table_header;
 
-<?php
-            if ($_SESSION['YACOMASVARS']['rootlevel']==1 ) {
-?>
+    foreach ($rooms as $room) {
+        $l_nombre = <<< END
+<a class="verde" href="Mlugar.php?idlugar={$room->id}">{$room->nombre_lug}</a>
+END;
+        $l_cupo = ($room->cupo != 99999) ? $room->cupo : '';
+        $l_eventos = <<< END
+<a class="azul" href="Leventos-lugar.php?idlugar={$room->id}">Eventos Registrados</a>
+END;
 
-    <td><a class="precaucion" href="Blugar.php?idlugar=<?=$room->id ?>">Eliminar</a></td>
+        $row_data = array($l_nombre, $room->ubicacion, $l_cupo, $l_eventos);
 
-<?php
-            }
-?>
-    </tr>
-
-<?php
-            $trclass = ($trclass=='even') ? 'odd' : 'even';
+        if ($rootlevel == 1) {
+            $l_action = <<< END
+<a class="precaucion" href="Blugar.php?idlugar={$room->id}">Eliminar</a>
+END;
+            $row_data = array_merge($row_data, array($l_action));
         }
+
+        $table_data[] = $row_data;
     }
+
+    do_table($table_data, 'wide');
+} else {
 ?>
-</table>
+
+<p class="error center">No se encontro ningún registro.</p>
+
+<?php } ?>
 
 <p id="buttons">
     <input type="button" value="Volver al menu" onClick="location.href='<?=$CFG->wwwroot ?>/admin/menuadmin.php#lugares'" />
 </p>
 
 <?php
-    do_footer();
+do_footer();
 ?>
