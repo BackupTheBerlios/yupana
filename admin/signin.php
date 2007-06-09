ca314361c392fad550b6b008af5df9f53953e85a
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-09 08:32:40 -0400

    minor update on login interface

--- admin/signin.php
+++ admin/signin.php
@@ -4,13 +4,14 @@
     global $CFG;
     global $db;
 
-    $errmsg = "";
 
-    $login = optional_param('S_login', '');
-    $pass = optional_param('S_passwd', '');
-    $submit = (optional_param('submit') == 'Iniciar') ? true : false;
-
+    $submit = optional_param('submit');
+    $login = optional_param('S_login');
+    $pass = optional_param('S_passwd');
     $exp = optional_param('e');
+
+
+    $errmsg = "";
 
     // check if has already session then redirect to main admin menu
     session_start();
@@ -20,7 +21,7 @@
     }
 
     // para poder autorizar la insercion del registro
-    if ($submit) {
+    if ($submit == "Iniciar") {
         if (!preg_match("/^\w{4,15}$/",$login) || empty($pass)) {
 
             $errmsg .= 'Usuario y/o password no válidos. Por favor trate de nuevo.';
@@ -54,8 +55,8 @@
     do_header();
 ?>
 
-    <h1>Módulo de administración</h1>
-    <h1>Inicio de Sesión</h1>
+<h1>Módulo de administración</h1>
+<h2 class="center">Inicio de Sesión</h2>
 
 <?php
 
@@ -63,31 +64,33 @@
 ?>
     <p class="error center"><?=$errmsg ?></p>
 
-<?php
-    }
-    elseif (isset($_GET['e']) && ($_GET['e'] == "exp")) { print '<span class="err">Su session ha caducado o no inicio session correctamente.  Por favor trate de nuevo.</span><p>'; }
-?>
-    <form method="POST" action="">
-        <div id="login-form" class="center">
-            <p>
-            <label for="S_login">Administrador: </label>
-            <input TYPE="text" name="S_login" size="15" value="<?=$login ?>" />
-            </p>
+<?php } elseif ($exp == "exp") { ?>
 
-            <p>
-            <label for="S_passwd">Contraseña: </label>
-            <input type="password" name="S_passwd" size="15" value="" />
-            </p>
+<p class="error center">Su session ha caducado o no inicio session correctamente.  Por favor trate de nuevo.<p>
 
-            <div id="buttons"> 
-                <input type="submit" name="submit" value="Iniciar">
-                <input type="button" value="Cancelar" onClick="location.href='../'">
-            </div>
+<?php } ?>
+
+<form method="POST" action="">
+    <div id="login-form" class="center">
+        <p>
+        <label for="S_login">Administrador: </label>
+        <input type="text" name="S_login" size="15" value="<?=$login ?>" />
+        </p>
+
+        <p>
+        <label for="S_passwd">Contraseña: </label>
+        <input type="password" name="S_passwd" size="15" value="" />
+        </p>
+
+        <div id="buttons"> 
+            <input type="submit" name="submit" value="Iniciar">
+            <input type="button" value="Cancelar" onClick="location.href='../'">
         </div>
-    </form>
+    </div>
+</form>
 
-    <p class="notice center">Las Cookies deben ser habilitadas pasado este punto.<br/>Su sesión caducará despues de 1 hora de inactividad.</p>
+<p class="notice center">Las Cookies deben ser habilitadas pasado este punto.<br/>Su sesión caducará despues de 1 hora de inactividad.</p>
 
 <?php
-    do_footer();
+do_footer();
 ?>
