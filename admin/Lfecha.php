b4fbf3f370f1db06ba14c33e721275eccd7bd9c5
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-09 03:56:10 -0400

    cleanup & update on listing dates

--- admin/Lfecha.php
+++ admin/Lfecha.php
@@ -1,53 +1,51 @@
-<? 
-include "../includes/lib.php";
-beginSession('R');
-imprimeEncabezado();
+<?php 
+    require_once('header-common.php');
 
-$link=conectaBD();
-$idadmin=$_SESSION['YACOMASVARS']['rootid'];
-$userQueryL = 'SELECT * FROM fecha_evento ORDER BY fecha';
-$userRecordsL = mysql_query($userQueryL) or err("No se pudo listar fechas".mysql_errno($userRecordsL));
-print '<P class="yacomas_login">Login: '.$_SESSION['YACOMASVARS']['rootlogin'].'&nbsp;<a class="precaucion" href=signout.php>Desconectarme</a></P>';
-imprimeCajaTop("100","Listado de fechas para eventos");
-print'
-	<table border=0 align=center width=100%>
-	<tr>
-	<td bgcolor='.$colortitle.'><b>Fecha</b></td><td bgcolor='.$colortitle.'><b>Descripcion</b></td>
-	<td bgcolor='.$colortitle.'><b>&nbsp;</b></td>';
-	
-	if ($_SESSION['YACOMASVARS']['rootlevel']==1 ) 
-		print '<td bgcolor='.$colortitle.'>&nbsp;</td>';
-	print '</tr>';
-	$color=1;
-	while ($fila = mysql_fetch_array($userRecordsL))
-	{
-		if ($color==1) 
-		{
-			$bgcolor=$color_renglon1;
-			$color=2;
-		}
-		else 
-		{
-			$bgcolor=$color_renglon2;
-			$color=1;
-		}
-		print '<tr>
-		<td bgcolor='.$bgcolor.'><a class="verde" href="Mfecha.php?idfecha='.$fila['id'].'">'.$fila['fecha'].'</td>
-		<td bgcolor='.$bgcolor.'>'.$fila['descr'].'</td>
-		<td bgcolor='.$bgcolor.'><a class="azul" href="Leventos-fecha.php?idfecha='.$fila['id'].'">Eventos registrados</td>';
-		
-		if ($_SESSION['YACOMASVARS']['rootlevel']==1 ) 
-		{
-			print '</td><td bgcolor='.$bgcolor.'><small><a class="precaucion" href="Bfecha.php?idfecha='.$fila['id'].'" onMouseOver="window.status=\'Eliminar fecha\';return true" onFocus="window.status=\'Eliminar fecha\';return true" onMouseOut="window.status=\'\';return true">Eliminar</a>';
-		}
-			print '</td></tr><tr><td>'; 
-		print '</tr>';
-	}
-	print '</table>';
-	retorno();
-	retorno();
-	print '<center>
-	<input type="button" value="Volver al menu" onClick=location.href="'.$fslpath.$rootpath.'/admin/menuadmin.php#lugares">
-	</center>';
-imprimeCajaBottom();
-imprimePie();?>
+    $rootlevel = $_SESSION['YACOMASVARS']['rootlevel'];
+?>
+
+<h1>Listado de fechas para eventos</h1>
+
+<?php
+$dates = get_records_sql('SELECT * FROM fecha_evento ORDER BY fecha');
+
+if (!empty($dates)) {
+    $table_data = array();
+
+    $table_header = array('Fecha', 'Descripción', 'Eventos');
+    if ($rootlevel == 1) {
+        $table_header = array_merge($table_header, array('Acción'));
+    }
+
+    $table_data[] = $table_header;
+
+    foreach ($dates as $date) {
+        $l_fecha = <<< END
+<a class="verde" href="Mfecha.php?idfecha={$date->id}">{$date->fecha}</a>
+END;
+        $l_eventos = <<< END
+<a class="azul" href="Leventos-fecha.php?idfecha={$date->id}">Eventos registrados</a>
+END;
+        $row_data = array($l_fecha, $date->descr, $l_eventos);
+
+        if ($rootlevel == 1) {
+            $l_action = <<< END
+<a class="precaucion" href="Bfecha.php?idfecha={$date->id}">Eliminar</a>
+END;
+            $row_data = array_merge($row_data, array($l_action));
+        }
+
+        $table_data[] = $row_data;
+    }
+
+    do_table($table_data, 'wide');
+}
+?>
+
+<p id="buttons">
+    <input type="button" value="Volver al menu" onClick="location.href='<?=$CFG->wwwroot ?>/admin/menuadmin.php#lugares'" />
+</p>
+
+<?php
+do_footer();
+?>
