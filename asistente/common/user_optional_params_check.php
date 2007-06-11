e3ac4514f484dfab6a3d743d56f1c67c1101dcc3
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-10 22:46:54 -0400

    common code for optional_params check

---
+++ asistente/common/user_optional_params_check.php
@@ -0,0 +1,62 @@
+<?php
+    // halt if running directly
+    if (empty($CFG)) {
+        die;
+    }
+
+    // check submit value
+    if ($submit == 'Registrarme' || $submit == 'Actualizar') {
+        // Verificar si todos los campos obligatorios no estan vacios
+        if (empty($login)
+            || empty($nombrep)
+            || empty($apellidos)
+            || empty($sexo)
+            || empty($id_estudios)
+            || empty($id_tasistente)
+            || empty($id_estado)) { 
+
+            $errmsg[] = "Verifica que los datos obligatorios los hayas introducido correctamente.";
+        }
+
+        if (!preg_match("/.+\@.+\..+/",$mail)) {
+            $errmsg[] = "El correo electrónico no es válido";
+        }
+
+        // Verifica que el login sea de al menos 4 caracteres
+        if (!preg_match("/^\w{4,15}$/",$login)) {
+            $errmsg[] = "El login que elijas debe tener entre 4 y 15 caracteres.";
+        }
+
+        if ($submit == 'Registrarme' || ($submit == 'Actualizar' && !empty($passwd))) {
+
+            // Verifica que el password sea de al menos 6 caracteres
+            if (!preg_match("/^.{6,15}$/",$passwd)) {
+                $errmsg[] = "El password debe tener entre 6 y 15 caracteres.";
+            }
+
+            // Verifica que el password usado no sea igual al login introducido por seguridad
+            elseif ($passwd == $login) {
+                $errmsg[] = "El password no debe ser igual a tu login.";
+            }
+
+            // Verifica que los password esten escritos correctamente para verificar que
+            // la persona introducjo correcamente el password que eligio.
+            if ($passwd != $passwd2) {
+                $errmsg[] = "Los passwords no concuerdan.";
+            }
+        }
+
+        // Si no hay errores verifica que el login no este ya dado de alta en la tabla
+        if (empty($errmsg)) {
+            $user = get_record('asistente', 'login', $login);
+
+            if (!(empty($user))
+                && ($submit == 'Registrarme'
+                    || ($submit == 'Actualizar' 
+                        && $user->id != $idasistente))) {
+                $errmsg[] = 'El usuario que elegiste ya ha sido tomado; por favor elige otro';
+            }
+        }
+    }
+
+?>
