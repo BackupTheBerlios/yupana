b907d8e22309681e83a634b74b189693a16c3009
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-11 18:12:31 -0400

    define home_url global

--- ponente/header-common.php
+++ ponente/header-common.php
@@ -2,6 +2,7 @@
     require_once(dirname(dirname(__FILE__)) . '/includes/lib.php');
 
     define('Context', 'ponente');
+    $home_url = $CFG->wwwroot . '/ponente';
 
     require($CFG->incdir . 'common/session.php');
 ?>
