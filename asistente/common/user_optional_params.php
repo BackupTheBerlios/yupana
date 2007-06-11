65db325106bdf7b99a174145027916d4c4acb87f
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-11 13:08:43 -0400

    common code centralized

--- asistente/common/user_optional_params.php
+++ asistente/common/user_optional_params.php
@@ -4,6 +4,7 @@
         die;
     }
 
+    // shared values
     $submit = optional_param('submit');
     $login = strtolower(optional_param('S_login'));
     $passwd = optional_param('S_passwd');
@@ -14,12 +15,20 @@
     $sexo = optional_param('C_sexo');
     $org = optional_param('S_org');
     $id_estudios = optional_param('I_id_estudios', 0, PARAM_INT);
-    $id_tasistente = optional_param('I_id_tasistente', 0, PARAM_INT);
     $ciudad = optional_param('S_ciudad');
     $id_estado = optional_param('I_id_estado', 0, PARAM_INT);
     $b_day = optional_param('I_b_day', 0, PARAM_INT);
     $b_month = optional_param('I_b_month', 0, PARAM_INT);
     $b_year = optional_param('I_b_year', 0, PARAM_INT);
+
+    // ponente values
+    $titulo = optional_param('S_titulo');
+    $domicilio = optional_param('S_domicilio');
+    $telefono = optional_param('S_telefono');
+    $resume = optional_param('S_resume');
+    
+    // asistente values
+    $id_tasistente = optional_param('I_id_tasistente', 0, PARAM_INT);
 
     // date of birth
     $fecha_nac = sprintf('%d-%02d-%02d', $b_year, $b_month, $b_day);
