b0595192f9739b5ea6925046b7174dad5c9fb750
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-11 18:09:46 -0400

    initialize home_url

--- asistente/header-common.php
+++ asistente/header-common.php
@@ -2,6 +2,7 @@
     require_once(dirname(dirname(__FILE__)) . '/includes/lib.php');
 
     define('Context', 'asistente');
+    $home_url = $CFG->wwwroot . '/asistente';
 
     require($CFG->incdir . 'common/session.php');
 ?>
