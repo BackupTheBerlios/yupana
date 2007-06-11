60a04d09de2dd719fc205dab7ac46ffb38c5b366
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-11 12:25:16 -0400

    common logout frontend

---
+++ logout.php
@@ -0,0 +1,44 @@
+<?php
+require('includes/lib.php');
+
+$context = optional_param('context');
+
+switch ($context) {
+    case 'admin':
+        $t = 'R';
+        $name = 'Administrador';
+        break;
+
+    case 'ponente':
+        $t = 'P';
+        $name = 'Ponente';
+        break;
+
+    case 'asistente':
+        $t = 'A';
+        $name = 'Asistente';
+        break;
+
+    default:
+        header('Location: ' . $CFG->wwwroot);
+}
+
+beginSession($t);
+
+// ignore erros
+@session_unset();
+@session_destroy();
+
+//start page
+do_header();
+?>
+
+<h1>Salida de sesión <?=$name ?></h1>
+<div class="block"></div>
+
+<p class="center">Ha salido exitosamente del sistema.</p>
+
+<?php
+do_submit_cancel('', 'Regresar', $CFG->wwwroot);
+do_footer();
+?>
