38031c418a3e28e221e441e843aacdabc6deb447
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-11 18:07:05 -0400

    new structure of index and links

--- asistente/index.php
+++ asistente/index.php
@@ -1,14 +1,42 @@
 <?php
-    require_once('../includes/lib.php');
+require('header-common.php');
 
-    $option = optional_param('opc');
+$q= optional_param('q');
+switch ($q) {
+    case 'kardex':
+        require($CFG->incdir . 'common/user_kardex.php');
+        break;
 
-    switch ($option) {
-        case NASISTENTE:
-            include "Nasistente.php";
-            break;
+    case 'edit':
+        require($CFG->incdir . 'common/user_edit.php');
+        break;
 
-        default:
-			include "signin.php";
-    }
+    default: // default index
+?> 
+
+<h1>Asistentes</h1>
+<h2>Bienvenido <?=$USER->nombrep ?> <?=$USER->apellidos ?></h2>
+
+<div id="menuadmin">
+    <div class="menuadmin column">
+        <ul>
+            <li><a href="<?=$CFG->wwwroot ?>/asistente/?q=edit">Modificar mis datos</a></li> 
+            <li><a href="<?=$CFG->wwwroot ?>/asistente/?q=kardex">Imprimir hoja de registro</a></li>
+            <li><a href="<?=$CFG->wwwroot ?>/asistente/?q=poll">Encuestas</a></li>
+        </ul>
+    </div>
+
+    <div class="menuadmin column">
+        <ul>
+            <li><a href="asistente.php?opc=<?=LEVENTOS ?>">Listas eventos programados</a></li>
+            <li><a href="asistente.php?opc=<?=LTALLERES ?>">Listar/Inscribirme a talleres y/o tutoriales</a></li>
+            <li><a href="asistente.php?opc=<?=LTALLERESREG ?>">Listar/Baja de talleres y/o tutoriales registrados</a></li>
+        </ul>
+    </div>
+</div>
+
+<?php 
+}
+
+do_footer();
 ?>
