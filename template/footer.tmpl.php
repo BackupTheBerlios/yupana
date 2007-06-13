03518e67dfdb5315c32885217bdb0864ffa291ab
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-13 19:30:33 -0400

    new default footer style

--- template/footer.tmpl.php
+++ template/footer.tmpl.php
@@ -1,38 +1,14 @@
-<!-- end body content -->
-</div><!-- #content -->
+    <!-- end body content -->
+    </div><!-- #content -->
 
-<div class="clear"></div>
+    <div class="clear"></div>
 
-<div id="footer">
-    <p>
-        <a href="http://yacomas.sourceforge.net/" title="Created with YACOMAS">
-            <img title="Created with YACOMAS" alt="GNU" src="<?=$CFG->wwwroot ?>/images/buttons/yacomas-micro.png" />
-        </a>
+    <div id="footer">
+        <p>Copyright (c) 2007, <a href="http://conference.web"><?=$CFG->conference_name ?></a>
+        <br />Powered by <a href="http://yacomas.sourceforge.net/">Yupana</a></p>
+    </div> <!-- #footer -->
 
-        <a href="http://www.php.net/" title="Pumped through PHP">
-            <img title="Pumped th/rough PHP" alt="PHP" src="<?=$CFG->wwwroot ?>/images/buttons/php.png" />
-        </a>
-
-        <a href="http://www.mysql.com/products/mysql/" title="MySQL under the hood">
-            <img title="MySQL under the hood" alt="MySQL" src="<?=$CFG->wwwroot ?>/images/buttons/mysql2.gif" />
-        </a>
-
-        <a href="http://www.gnu.org/" title="Fueled by GNU software">
-            <img title="Fueled by GNU software" alt="GNU" src="<?=$CFG->wwwroot ?>/images/buttons/gnu-powered.png" />
-        </a>
-
-        <br />
-
-        Copyright (c) 2007, <a href="http://conference.web">Conference comite</a>
-
-        <br />
-
-        Powered by <a href="http://yacomas.sourceforge.net/">Yacomas</a>
-        - <a href="mailto:patux@patux.net">Report a bug</a>
-
-    </p>
-   
-</div> <!-- #footer -->
 </div> <!-- #container -->
+
 </body>
 </html>
