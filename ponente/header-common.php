1b5d128b1664685343227a855e4cc208f79b1572
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-12 21:34:08 -0400

    removed unneeded ponente code

--- ponente/header-common.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once(dirname(dirname(__FILE__)) . '/includes/lib.php');
-
-    define('Context', 'ponente');
-    $home_url = $CFG->wwwroot . '/ponente';
-
-    require($CFG->incdir . 'common/session.php');
-?>
