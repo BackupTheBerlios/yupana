2b242f08a86c627cbe788728d1d0c23587eef6d6
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-15 15:39:47 -0400

    updated the format of proposal view

--- includes/common/prop_display_info.php
+++ includes/common/prop_display_info.php
@@ -4,59 +4,53 @@
         die;
     }
    
-    // flag prop_noshow_resume
+    // initalize var
+    $values = array();
 
-    $values = array(
-        'Nombre de Ponencia' => $proposal->ponencia,
-        'Nivel' => $proposal->nivel,
+    if (Action == 'updateproposal' || Action == 'deleteproposal') {
+        $values['Nombre de ponencia'] = $proposal->nombre;
+    }
+
+    // flag prop_noshow_resume
+    if (empty($prop_noshow_resume)) {
+        $values['Resumen'] = nl2br(htmlspecialchars($proposal->resumen));
+    }
+
+    $values = array_merge($values, array(
         'Tipo de Propuesta' => $proposal->tipo,
         'Orientación' => $proposal->orientacion,
         'Duración' => sprintf('%d Hrs.', $proposal->duracion),
-//        'Status' => $proposal->status
-        );
+        'Nivel' => $proposal->nivel,
+        ));
 
     if (!defined('Action') || (Action != 'newproposal' && Action != 'updateproposal')) {
         $values['Status'] = '<b>' . $proposal->status . '</b>';
     }
 
     if (Context == 'ponente' && !empty($proposal->id_administrador)) {
-        $contactmail = get_field('administrador', 'mail', 'id', $proposal->id_administrador);
-        $contactmail = sprintf("<em>%s</em>", $contactmail);
-
-        $values = array_merge($values,
-                 array('Correo de contacto' => $contactmail));
-    }
-
-    // if it's schedule merge date info
-    if ($proposal->id_status == 8) {
-        $query = '
-            SELECT FE.fecha, L.nombre_lug AS lugar, EO.hora
-            FROM evento E
-            JOIN evento_ocupa EO ON EO.id_event = E.id
-            JOIN fecha_evento FE ON FE.id = E.id_fecha
-            JOIN lugar L ON L.id = E.id_lugar
-            WHERE E.id_propuesta = '.$proposal_id;
-
-        $schedule = get_record_sql($query);
-        $endhour = $schedule->hora + $proposal->duracion -1;
-        $schedule->time = sprintf('%02d:00 - %02d:50', $schedule->hora, $endhour);
+        $contactmail = sprintf('<em>%s</em>', $proposal->adminmail);
 
         $values = array_merge($values, array(
-                'Fecha' => $schedule->fecha,
-                'Lugar' => $schedule->lugar,
-                'Hora' => $schedule->time
+                'Correo de contacto' => $contactmail
             ));
     }
 
-    // show proposal info
     do_table_values($values, 'narrow');
+
+    // if it's schedule merge date info
+    if ($proposal->id_status == 8) {
+        $values = array(
+                'Fecha' => $proposal->human_date,
+                'Lugar' => $proposal->lugar,
+                'Hora' => $proposal->time
+            );
+
+        do_table_values($values, 'narrow');
+    }
 
     if (empty($prop_noshow_resume)) {
         // reset values
         $values = array();
-
-        // show proposal aditional info
-        $values['Resumen'] = $proposal->resumen;
 
         if (Context == 'ponente') {
             $values['Requisitos técnicos del taller'] = $proposal->reqtecnicos;
