4b33c52e1f35c2301fca203c30ceb34dd35eac12
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-15 17:25:10 -0400

    new style on proposal view

--- includes/common/prop_view.php
+++ includes/common/prop_view.php
@@ -4,55 +4,45 @@ if (empty($q) || empty($CFG)) {
     die;
 }
 
-// safe default value
-$where = '1!=1';
-
 // where we are?
 if (Context == 'ponente') {
     preg_match('#^speaker/proposals/(\d+)/?$#', $q, $matches);
     $proposal_id = (!empty($matches)) ? (int) $matches[1] : 0;
 
-    $where = 'P.id = '. $proposal_id .' AND id_ponente = '. $USER->id;
+    $proposal = get_proposal($proposal_id, $USER->id);
 }
 
 elseif (Context == 'main') {
-    preg_match('#^general/proposals/(\d+)$#', $q, $matches);
+    preg_match('#^general/proposals/(\d+)/?$#', $q, $matches);
     $proposal_id = (!empty($matches)) ? (int) $matches[1] : 0;
 
-    $where = 'P.id = '. $proposal_id;
+    $proposal = get_proposal($proposal_id);
 }
-
-$query = '
-    SELECT PO.id AS id_ponente, PO.nombrep, PO.apellidos,
-    P.nombre AS ponencia,
-    P.duracion,
-    P.id_status,
-    P.resumen,
-    P.reqasistente,
-    P.reqtecnicos,
-    P.id_administrador,
-    PN.descr AS nivel,
-    PT.descr AS tipo,
-    O.descr AS orientacion,
-    S.descr AS status
-    FROM propuesta P
-    JOIN ponente PO ON P.id_ponente = PO.id
-    JOIN prop_nivel PN ON P.id_nivel = PN.id
-    JOIN prop_tipo PT ON P.id_prop_tipo = PT.id
-    JOIN orientacion O ON P.id_orientacion = O.id
-    JOIN prop_status S ON P.id_status = S.id
-    WHERE '. $where;
-
-$proposal = get_record_sql($query);
 
 if (!empty($proposal)) {
 ?>
 
-<h1>Ponencia de: <a href="<?=get_url('general/authors/'.$proposal->id_ponente) ?> ?>"><?=$proposal->nombrep ?> <?=$proposal->apellidos ?></a></h1>
+<h1 class="proposal-title left"><?=$proposal->nombre ?></h1>
+<h2 class="proposal-details left">Detalles de la ponencia</h2>
 
 <?php
     include($CFG->comdir . 'prop_display_info.php');
 
+    if (Context != 'ponente') {
+?>
+
+<h2 class="proposal-details left">Detalles de los autores</h2>
+
+<ul>
+<li>
+    <p><?=$proposal->nombrep ?> <?=$proposal->apellidos ?></p>
+    <p><?=$proposal->org ?></p>
+    <p><?=nl2br($proposal->resume) ?></p>
+</li>
+</ul>
+
+<?php
+     }
 } else {
 ?>
 
@@ -64,11 +54,12 @@ if (!empty($proposal)) {
 
 <p class="error center">No se encontro la propuesta en tus registros.</p>
 
-<?php } ?>
+<?php } else { ?>
 
 <p class="error center">Registros de propuesta no encontrados. Posiblemente no exista
 o no tengas acceso al registro.</p>
 
 <?php
+    }
 }
 ?>
