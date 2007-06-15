d2690f0df559b6c13d37173841fc1585380a900e
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-15 10:52:44 -0400

    fixed issues on get_* functions

--- includes/infolib.php
+++ includes/infolib.php
@@ -1,13 +1,27 @@
 <?php
 
-function get_info($where, $type='person', $limit='', $order='P.id, P.reg_time') {
+function get_info($where, $type='person', $limit='', $order='') {
     $record = new StdClass;
+
+    // hide deleted proposals to non admins
+    if ($type == 'proposal' && Context != 'admin') {
+        $where .= ' AND P.id_status != 7';
+    }
 
     if (!empty($limit) && $limit != 1) {
         $where .= ' LIMIT ' . $limit;
     }
 
-    if (!empty($order)) {
+    if (empty($order)) {
+        //default order
+        if ($type == 'proposal') {
+            $where .= ' ORDER BY P.id, P.reg_time';
+        } 
+            
+        elseif ($type == 'speaker') {
+            $where .= ' ORDER BY SP.id, SP.reg_time';
+        }
+    } else {
         $where .= ' ORDER BY ' . $order;
     }
 
@@ -43,10 +57,10 @@ function get_info($where, $type='person', $limit='', $order='P.id, P.reg_time') 
     }
 
     // proposals?
-    elseif ($type == 'proposal' && $limit != 1) {
+    elseif ($type == 'proposal') {
         $query = '
             SELECT P.*, L.descr AS nivel, SP.login, SP.nombrep,
-            SP.apellidos, T.descr AS tipo, ADM.login AS administrador,
+            SP.apellidos, SP.org, SP.resume, T.descr AS tipo, ADM.mail AS adminmail,
             O.descr AS orientacion, S.descr AS status
             FROM propuesta P
             JOIN prop_nivel L ON L.id = P.id_nivel
@@ -58,30 +72,28 @@ function get_info($where, $type='person', $limit='', $order='P.id, P.reg_time') 
             WHERE ' . $where;
     }
 
-    // one proposal, get event details
-    elseif ($type == 'proposal' && $limit == 1) {
-        $query = '
-            SELECT P.*, L.descr AS nivel, SP.login, SP.nombrep,
-            SP.apellidos, T.descr AS tipo, ADM.login AS administrador,
-            O.descr AS orientacion, S.descr AS status,
-            FE.fecha, R.nombre_lug AS lugar, EO.hora
-            FROM propuesta P
-            JOIN prop_nivel L ON L.id = P.id_nivel
-            JOIN ponente SP ON SP.id = P.id_ponente
-            JOIN prop_tipo T ON T.id = P.id_prop_tipo
-            LEFT JOIN administrador ADM ON ADM.id = P.id_administrador
-            JOIN orientacion O ON O.id = P.id_orientacion
-            JOIN prop_status S ON S.id = P.id_status
-            LEFT JOIN evento E ON E.id_propuesta = P.id
-            LEFT JOIN evento_ocupa EO ON EO.id_evento = E.id
-            LEFT JOIN fecha_evento FE ON FE.id = E.id_fecha
-            LEFT JOINT lugar R ON R.id = E.id_lugar
-            WHERE ' . $where;
-    }
-
     // get only one record?
     if ($limit == 1) {
         $records = get_record_sql($query);
+
+        //get event if programmed 
+        if (!empty($records) && $type == 'proposal' && $records->id_status == 8) {
+            $query = '
+                SELECT FE.fecha, R.nombre_lug AS lugar, EO.hora
+                FROM evento E
+                JOIN fecha_evento FE ON FE.id = E.id_fecha
+                JOIN lugar R ON R.id = E.id_lugar
+                JOIN evento_ocupa EO ON EO.id_evento = E.id
+                WHERE E.id_propuesta=' . $records->id;
+
+            $event = get_record_sql($query);
+            $endhour = $evento->hora + $records->duracion -1;
+
+            $records->fecha = $evento->fecha;
+            $records->lugar = $evento->lugar;
+            $records->time = sprintf('%02d:00 - %02d:50', $evento->hora, $endhour);
+        }
+
     } else {
         $records = get_records_sql($query);
     }
@@ -94,15 +106,15 @@ function get_admins($limit='') {
 }
 
 function get_speakers($limit='') {
-    return get_info('1=1', 'speakers', $limit);
+    return get_info('1=1', 'speaker', $limit);
 }
 
 function get_persons($limit='') {
     return get_info('1=1', 'person', $limit);
 }
 
-function get_proposals($limit='') {
-    return get_info('1=1', 'proposal', $limit);
+function get_proposals($where='1=1', $limit='', $order='') {
+    return get_info($where, 'proposal', $limit, $order);
 }
 
 function get_admin($id) {
@@ -117,8 +129,14 @@ function get_person($id) {
     return get_info('P.id='.$id, 'person', 1);
 }
 
-function get_proposal($id) {
-    return get_info('P.id='.$id, 'proposal', 1);
+function get_proposal($id, $id_speaker=0) {
+    $where = 'P.id='.$id;
+
+    if (!empty($id_speaker)) {
+        $where .= ' AND P.id_ponente='.$id_speaker;
+    }
+
+    return get_info($where, 'proposal', 1);
 }
 
 function check_login($login, $pass, $type='person') {
