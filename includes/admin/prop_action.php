bf6ee8c5deddf59f7eaa65c82d67f234f525fb04
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-16 16:15:50 -0400

    prop_action, change the status of proposals

---
+++ includes/admin/prop_action.php
@@ -0,0 +1,37 @@
+<?php
+
+if (empty($CFG) || empty($q) || Context != 'admin') {
+    die;
+}
+
+if (preg_match('#^admin/proposals/deleted/\d+/status/\d+/?#', $q)) {
+    preg_match('#^admin/proposals/deleted/(\d+)/status/(\d+)#', $q, $matches);
+    $prop_id = (int) $matches[1];
+    $prop_id_status = (int) $matches[2];
+
+    $return_url = get_url('admin/proposals/deleted');
+} 
+
+elseif (preg_match('#^admin/preg_match/\d+/status/\d+/?')){
+    preg_match('#^admin/proposals/(\d+)/status/(\d+)#', $q, $matches);
+    $prop_id = (int) $matches[1];
+    $prop_id_status = (int) $matches[2];
+
+    $return_url = get_url('admin/proposals');
+}
+
+// update reg flag status
+if (!empty($prop_id) && !empty($prop_id_status)) {
+
+    $prop = new StdClass;
+    $prop->id = $prop_id;
+    $prop->id_status = $prop_id_status;
+
+    if ($rs = update_record('propuesta', $prop)) {
+        $errmsg[] = 'Se ha actualizado el estado de la ponencia';
+    } else {
+        $errmsg[] = 'Ocurrió un error al cambiar el estado del registro';
+    }
+}
+
+header('Location: ' . $return_url);
