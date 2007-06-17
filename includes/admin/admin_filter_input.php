95c5ca12ea6e81b29b06db5416b6dee28079b673
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-17 01:38:31 -0400

    fixed issue with quotes/slashes

--- includes/admin/admin_filter_input.php
+++ includes/admin/admin_filter_input.php
@@ -1,8 +1,10 @@
 <?php
+$onChange = 'onChange=\'form.submit()\'';
+
 // only filter by tadmin
 $admin_types = get_records('tadmin');
 
-$admin_input = do_get_output('do_input_select', array('filter_id_tadmin', $admin_types, $id_tadmin, true, '', 0, 'onChange=\'form.submit()\''));
+$admin_input = do_get_output('do_input_select', array('filter_id_tadmin', $admin_types, $id_tadmin, true, '', 0, $onChange));
 
 $table_data = array();
 $table_data[] = array('', 'Tipo:');
