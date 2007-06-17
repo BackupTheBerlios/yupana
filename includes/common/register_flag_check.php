b961d33e6ac813919a768564c44841a453779ed2
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-17 01:41:05 -0400

    completed register flag check

--- includes/common/register_flag_check.php
+++ includes/common/register_flag_check.php
@@ -4,21 +4,33 @@
         die;
     }
 
-    if (Context == 'ponente') {
+    if (Context == 'ponente' && Action == 'register') {
         $id = 1; // REGPONENTES
         $name = 'ponentes';
-    } elseif (Context == 'asistente') {
+    } elseif (Context == 'asistente' && Action == 'register') {
         $id = 2; // REGASISTENTES
         $name = 'asistentes';
+    } elseif (Context == 'ponente' && Action == 'newproposal') {
+        $id = 3; // REGPONENCIAS
+        $name = 'ponencias';
+    } elseif (Context == 'asistente' && Action == 'workshopregister') {
+        $id = 4; // INSCRIP
+        $name = 'talleres/tutoriales';
     } else {
-        die; //FIXME
+        if (Context == 'admin') {
+            $open_flag = true;
+        } else {
+            die; // this never should be happen
+        }
     }
 
-    // Check if register is open 
-    $open_flag = get_field('config', 'status', 'id', $id);
+    if (Context != 'admin') {
+        // Check if register is open 
+        $open_flag = get_field('config', 'status', 'id', $id);
+    }
 
     // if not open, end page
-    if (!$open_flag && Context != 'admin') {
+    if (!$open_flag) {
 ?>
 
 <div class="block"></div>
@@ -30,3 +42,4 @@
         do_footer();
         exit;
     }
+?>
