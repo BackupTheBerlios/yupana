22919c3f1e03e2ddb800574bc9544988a7ccb484
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-17 02:37:41 -0400

    improvements on filters

--- includes/common/prop_filter_input.php
+++ includes/common/prop_filter_input.php
@@ -1,22 +1,88 @@
 <?php
 // dont show organization events
-$prop_type = get_records_select('prop_tipo', 'id <= 100');
+//$prop_type = get_records_select('prop_tipo', 'id <= 100');
+$onChange = 'onChange=\'form.submit()\'';
 
-// get all tracks
-$tracks = get_records('orientacion');
+if (Context == 'admin') {
+    $prop_select = ' AND P.id_status != 7';
+} else {
+    $prop_select = '';
+}
+
+$prop_type = get_records_sql('SELECT PT.* FROM prop_tipo PT JOIN propuesta P ON P.id_prop_tipo=PT.id WHERE PT.id <= 100'.$prop_select);
 
 // dont show deleted status
-$status = get_records_select('prop_status', 'id != 7');
+if (Context == 'admin') {
+    //dont show programmed
+    $status_select = 'S.id < 7';
+} else {
+    $status_select = 'S.id != 7';
+}
 
-$prop_type_input = do_get_output('do_input_select', array('filter_id_prop_tipo', $prop_type, $id_prop_tipo, true, '', 0, 'onChange="form.submit()"'));
-$tracks_input = do_get_output('do_input_select', array('filter_id_orientacion', $tracks, $id_orientacion, true, '', 0, 'onChange="form.submit()"'));
-$status_input = do_get_output('do_input_select', array('filter_id_status', $status, $id_status, true, '', 0, 'onChange="form.submit()"'));
+//$status = get_records_select('prop_status', $select);
+$status = get_records_sql('SELECT S.* FROM prop_status S JOIN propuesta P ON P.id_status=S.id WHERE '.$status_select.' ORDER BY S.id');
+
+$prop_type_input = do_get_output('do_input_select', array('filter_id_prop_tipo', $prop_type, $id_prop_tipo, true, '', 0, $onChange));
+
+$status_input = do_get_output('do_input_select', array('filter_id_status', $status, $id_status, true, '', 0, $onChange));
 
 $table_data = array();
 
-//headers
-$table_data[] = array('', 'Tipo:', 'Orientacion:', 'Estado:');
-$table_data[] = array('Filtro:', $prop_type_input, $tracks_input, $status_input);
+if (Context == 'admin') {
+    $admins = get_records_sql('SELECT ADM.id, ADM.login as descr FROM administrador ADM JOIN propuesta P ON P.id_administrador = ADM.id');
+
+    $none = new StdClass;
+    $none->id = -1;
+
+    if (Action == 'listproposals') {
+        $none->descr = 'Ninguno';
+    } else {
+        $none->descr = 'Usuario';
+    }
+
+    // put at the top "none"
+    $admins = array_merge(array($none), $admins);
+
+    $admins_input = do_get_output('do_input_select', array('filter_id_adminlogin', $admins, $id_admin, true, '', 0, $onChange));
+
+    if (Action == "listproposals") {
+        $table_data[] = array('', 'Tipo:', 'Estado:', 'Asignado:');
+        $table_data[] = array('Filtro:', $prop_type_input, $status_input, $admins_input);
+    }
+
+    elseif (Action == 'listdeletedproposals') {
+        $speakers = get_records_sql('SELECT SP.id, SP.login AS descr FROM ponente SP JOIN propuesta P ON P.id_ponente = SP.id WHERE P.id_status=7');
+
+        $speakers_input = do_get_output('do_input_select', array('filter_id_ponente', $speakers, $id_ponente, true, '', 0, $onChange));
+
+        $table_data[] = array('', 'Modificado por:', 'Tipo:', 'Ponente:');
+        $table_data[] = array('Filtro:', $admins_input, $prop_type_input, $speakers_input);
+    }
+
+    elseif (Action == 'newevent') {
+        // acepted proposals
+        $prop_type = get_records_sql('SELECT PT.* FROM prop_tipo PT JOIN propuesta P ON P.id_prop_tipo=PT.id WHERE P.id_status=5');
+        $prop_type_input = do_get_output('do_input_select', array('filter_id_prop_tipo', $prop_type, $id_prop_tipo, true, '', 0, $onChange));
+
+        $track = get_records_sql('SELECT O.* FROM orientacion O JOIN propuesta P ON P.id_orientacion=O.id WHERE P.id_status=5');
+        $track_input = do_get_output('do_input_select', array('filter_id_orientacion', $track, $id_orientacion, true, '', 0, $onChange));
+
+        $table_data[] = array('', 'Tipo:', 'Orientación:');
+        $table_data[] = array('Filtro:', $prop_type_input, $track_input);
+    }
+}
+
+else {
+    // get all tracks
+    //$tracks = get_records('orientacion');
+    $tracks = get_records_sql('SELECT O.* FROM orientacion O JOIN propuesta P ON P.id_orientacion=O.id');
+
+    $tracks_input = do_get_output('do_input_select', array('filter_id_orientacion', $tracks, $id_orientacion, true, '', 0, $onChange));
+
+    //headers
+    $table_data[] = array('', 'Tipo:', 'Orientacion:', 'Estado:');
+    $table_data[] = array('Filtro:', $prop_type_input, $tracks_input, $status_input);
+}
 
 do_table($table_data, 'prop-filter wide');
 ?>
--- includes/common/prop_filter_optional_params.php
+++ includes/common/prop_filter_optional_params.php
@@ -5,6 +5,26 @@
     $id_orientacion = optional_param('filter_id_orientacion', 0, PARAM_INT);
     $id_status = optional_param('filter_id_status', 0, PARAM_INT);
 
+    if (Context == 'admin') {
+        $id_admin = optional_param('filter_id_adminlogin', 0, PARAM_INT);
+
+        if (!empty($id_admin)) {
+            if ($id_admin == -1) {
+                $where .= ' AND P.id_administrador=0';
+            } else {
+                $where .= ' AND P.id_administrador='.$id_admin;
+            }
+        }
+
+        if (Action == 'listdeletedproposals') {
+            $id_ponente = optional_param('filter_id_ponente', 0, PARAM_INT);
+
+            if (!empty($id_ponente)) {
+                $where .= ' AND P.id_ponente='.$id_ponente;
+            }
+        }
+    }
+
     // modify sql where
     if (!empty($id_prop_tipo)) {
         $where .= ' AND P.id_prop_tipo='.$id_prop_tipo;
