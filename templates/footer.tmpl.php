1f05d8a836a7bbf3cece211c05a60fb477831028
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-23 00:29:00 -0400

    updated footer info

--- templates/footer.tmpl.php
+++ templates/footer.tmpl.php
@@ -4,8 +4,9 @@
     </div><!-- #content -->
 
     <div id="footer">
-        <p>Copyright (c) 2007, <a href="http://conference.web"><?=$CFG->conference_name ?></a>
-        <br />Powered by <a href="#">Yupana</a></p>
+        <p>Copyright (c) 2007, <a href="<?=$CFG->conference_link ?>"><?=$CFG->conference_name ?></a>
+        <br />Powered by <a href="http://yupana.berlios.de">Yupana</a></p>
+        <!-- copyright (c) 2007 - Rolando Espinoza La Fuente <darkrho at gmail.com> -->
     </div> <!-- #footer -->
 
 </div> <!-- #container -->
