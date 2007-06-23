d205767b91816bcdd1037c5a5112c975957d8e25
Rolando Espinoza La Fuente <darkrho@gmail.com>
2007-06-23 00:28:47 -0400

    updated header info

--- templates/header.tmpl.php
+++ templates/header.tmpl.php
@@ -6,15 +6,16 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta http-equiv="Content-Language" content="es" />
     <meta http-equiv="Pragma" content="no-cache" />
+    <meta name="generator" content="Yupana" />
     <meta name="robots" content="index,follow" />
-    <meta NAME="audience" CONTENT="All">
-    <meta NAME="distribution" content="Global">
-    <meta NAME="rating" content="General">
-    <meta name="revisit-after" content="1 days">
-    <meta name="description" content="<?=$CFG->conference_name ?>">
-    <meta NAME="keywords" content="congreso,bolivia,festival,software,libre,software libre,festival de software libre,2004,FSL,linux,gnu,gpl,openbsd,freebsd,netbsd,gnu/linux">
-    <link rel="icon" href="<?=get_url() ?>/images/yacomas.ico">
-    <link rel="stylesheet" type="text/css" href="<?=$CFG->stylesheet ?>" media="all">
+    <meta name="audience" content="All" />
+    <meta name="distribution" content="Global" />
+    <meta name="rating" content="General" />
+    <meta name="revisit-after" content="1 days" />
+    <meta name="description" content="<?=$CFG->conference_name ?>" />
+    <meta name="keywords" content="congreso,bolivia,festival,software,libre,software libre,festival de software libre,linux,gnu,gpl,openbsd,freebsd,netbsd,gnu/linux, yupana, conference, management, system" />
+    <link rel="icon" href="<?=get_url() ?>/images/yacomas.ico" />
+    <link rel="stylesheet" type="text/css" href="<?=$CFG->stylesheet ?>" media="all" />
 </head>
 
 <body>
